5d053b45005517408da642224f57c6a95e12569c
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-11-04 14:07:27 -0400

    Add test cases for breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,20 @@
-const args = process.argv;
-let arg = args.slice(2);
-
 const request = require('request');
-request(`https://api.thecatapi.com/v1/breeds/search?q=${arg[0]}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log("Breed not found.");
-  }
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback("Breed not found.", null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
 
